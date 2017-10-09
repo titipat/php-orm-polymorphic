331891457b4edd03e38e57255f85915cc1ba7880
Titipat <ttpsvb@gmail.com>
2017-10-09 11:39:25 +0700

    Seed users with faker

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker\Factory::create();
+
+        for ($i = 0; $i < 5; $i++)
+        {
+            $firstName = $faker->firstName;
+            $lastName = $faker->lastName;
+            DB::table('users')->insert([
+                'id' => ($i + 1),
+                'first_name' => $firstName,
+                'last_name' => $lastName,
+                'email' => strtolower("{$firstName}@example.com")
+            ]);
+        }
+    }
+}
